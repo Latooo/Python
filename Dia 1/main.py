273d3b5ecd5436d7a8e8ef9c9b1ec78a5ef53053
Latooo <daniellatorre600@gmail.com>
2024-01-25 09:24:16 -0500

    Trabajo de clase

--- Dia 1/main.py
+++ Dia 1/main.py
@@ -1,1 +1,63 @@
-"hola"
+## ------------------------------------------
+## ------ Ejercicio 1 ------
+## ------------------------------------------
+
+#Impresion en consola 
+print("Hola Mundo")
+
+# ------ Datos primitivos ------
+
+#1. String
+texto = "Campus"
+print (texto)
+print(type(texto))
+#2. Int
+Numero = 10
+print(Numero)
+print (type(Numero))
+#3. Float
+Decimal = 3.5
+print(Decimal)
+print (type(Decimal))
+#4. Double
+DecimalDouble = 3.141632132132133213
+print (DecimalDouble)
+print (type(DecimalDouble))
+#5. Boolean
+booleano = True
+print (booleano)
+print (type(booleano))
+
+# ------ Entradas parte del usuario ------
+
+entradaUsuario = input("Ingresa tu nombre ")
+print ("Tu nombre es ", entradaUsuario)
+
+# ------ Entradas parte del usuario con definicion de tipo de dato primitivo ------
+
+entradaUsuarioNum = input("Ingresa tu edad ")
+numeroFinal = int(entradaUsuarioNum)
+print ("Tu edad es ", numeroFinal)
+
+# ------ Ciclos ------
+# Ciclo for
+for i in range (5,10,2) : # for i in range (desde, hasta, pasos) :
+    print (i)
+
+# Ciclo while
+booleanito = True
+while booleanito == True :
+    print ("sigo vivo")
+    booleanito = False
+
+# ------ Condicionales ------
+
+texto1 = "campus"
+if texto1 == "campus" :
+    print ("Soy camper")
+else :
+    print ("No soy camper")
+    
+## Desarrollado por DANIEL ALEJANDRO LATORRE RUIZ - CC 1005257201
+
+
