4d5fec15b9387d27f9d57ea51e173f887060d1eb
Daniel Latorre <daniellatorre600@gmail.com>
2024-02-02 06:58:17 -0500

    finalizado

--- Dia 6/Ejercicio3_LatorreDaniel.py
+++ Dia 6/Ejercicio3_LatorreDaniel.py
@@ -1,19 +1,32 @@
-def ball_collide(ball1, ball2):
-    
-    # Extraer coordenadas y radio de cada bola
-    x1, y1, r1 = ball1
-    x2, y2, r2 = ball2
-    
-    # Calcular la distancia entre los centros de las bolas
-    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-    
-    # Verificar si las bolas están colisionando
-    if distance <= (r1 + r2):
-        return True
-    else:
-        return False
+import math
 
-# Ejemplo de uso
-ball1 = (1, 2, 3)  # Bola 1 con centro en (1, 2) y radio 3
-ball2 = (4, 5, 2)  # Bola 2 con centro en (4, 5) y radio 2
-print(ball_collide(ball1, ball2))  # Output: False (las bolas no están colisionando)
+#2D
+
+def Colision(bola1, bola2):
+    x1, y1, r1 = bola1
+    x2, y2, r2 = bola2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance <= r1 + r2
+
+bola1 = (0,0,5)
+bola2 = (8,0,3)
+
+print(Colision(bola1, bola2))
+
+#3D
+
+def Colision(bola1, bola2):
+    x1, y1, z1, r1 = bola1
+    x2, y2, z2, r2 = bola2
+    distance_euclides = math.sqrt((x2 - x1)**2 + (y2 - y1)) + (z2 - z1)**2
+    print(distance_euclides)
+    return distance_euclides <= r1 + r2
+
+bola1 = (2, 2, 2, 2)
+bola2 = (6.5, 2, 2, 2)
+
+print(Colision(bola1, bola2))
+
+
+##Desarollado por Joel Santiago Sierra Leon - 1097492869
+# Ayudantes Juan David Rivero - Kenia Hernandez - Paula - Andres Daza - Daniel Latorre 
